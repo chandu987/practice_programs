2089c1e9cde37794cbe3ab5402e57210c8d572aa
Chandu <tchandana03gmail.com>
2021-02-20 19:40:50 +0530

    my second message

--- src/main/java/com/core/practice/MyFirstProgram.java
+++ src/main/java/com/core/practice/MyFirstProgram.java
@@ -3,5 +3,6 @@ package com.core.practice;
 public class MyFirstProgram {
     public static void main(String[] args) {
         System.out.println("my first program");
+        System.out.println("my second message");
     }
 }
